d26c9cdad65b96596138d33deaf22930d9dea191
NamgyungKim <namgyung.kim@gmail.com>
2022-05-13 09:37:47 +0900

    🎉 Create project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  {{ msg }}
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: 'Hello Webpack!!',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
